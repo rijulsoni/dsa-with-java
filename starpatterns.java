0cc26aa79a35f2b4d59ee065fcea71cf48215033
rijulsoni <70277438+rijulsoni@users.noreply.github.com>
2024-03-03 15:41:25 +0530

    java pattern question part 1

---
+++ starpatterns.java
@@ -0,0 +1,140 @@
+public class starpatterns {
+    public static void main(String[] args) {
+
+        // Pattern 1: Print a line of asterisks
+        // *******
+        // int count = 6;
+        // for(int i=0;i<=count;i++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+
+        // Pattern 2: Print a square of asterisks
+        // *******
+        // *******
+        // *******
+        // *******
+        // *******
+        // *******
+        // *******
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = 1; j <= count; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Pattern 3: Print a triangle of asterisks
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+        // ******
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Pattern 4: Print a triangle of numbers
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+        // 123456
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        // Pattern 5: Print a triangle of repeating numbers
+        // 1
+        // 22
+        // 333
+        // 4444
+        // 55555
+        // 666666
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(i);
+        // }
+        // System.out.println();
+        // }
+
+        // Pattern 6: Print a reversed triangle of asterisks
+        // ******
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = count; j >= i; j--) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Pattern 7: Print a triangle of numbers in increasing order
+        // 123456
+        // 23456
+        // 3456
+        // 456
+        // 56
+        // 6
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = i; j <= count; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        // Pattern 8: Print a pyramid of asterisks
+        //      *
+        //     ***
+        //    *****
+        //   *******
+        //  *********
+        // ***********
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = count; j >= i; j--) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= 2 * i - 1; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Pattern 9: Print an inverted pyramid of asterisks
+        // ***********
+        //  *********
+        //   *******
+        //    *****
+        //     ***
+        //      *
+        // int count = 6;
+        // for (int i = 1; i <= count; i++) {
+        // for (int j = 1; j <= i-1; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 2 * count-i; j >= i; j--) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+    }
+}
